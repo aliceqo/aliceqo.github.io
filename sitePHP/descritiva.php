247b71d20cecd03b03e5ee1aa7b6f332a4e494ac
Alice Queiroz <124536945+aliceqo@users.noreply.github.com>
2024-11-01 15:31:13 -0300

    Add files via upload

---
+++ sitePHP/descritiva.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/descritiva.css">
+    <title>Descritiva</title>
+</head>
+<body>
+    <header>
+        <a href="index.php">Início</a>
+        <a href="conteudo01.php">Ingressos</a>
+        <a href="conteudo02.php">Tipos de Ingressos</a>
+        <a href="contato.php">Contatos</a>
+        <a href="descritiva.php">Descrição</a>
+    </header>
+
+    <div class="descricao">
+        <h1>Descrição do site</h1>
+        <h2>
+            Alice Queiróz:
+            conteudo02.php, conteudo.css, descritiva.css
+        </h2>
+        <p>1- Controle de sessions:
+            <br>
+            Foi utilizada quando o usuário clica no botão para comprar um ingresso, é registrado o tipo do ingresso comprado. Após a "compra", uma mensagem de sucesso aparece, mas o sistema a destrói quando a página é atualizada.
+            <br><br>
+            2- Matriz:
+            <br>
+            Os tipos de ingressos disponíveis foram organizados em forma de lista.
+            <br><br>
+            3- Formulários:
+            <br>
+            Foram utilizados formulários com método POST para a entrada de conteúdo dos ingressos (título, benefícios, preço e uma imagem ilustrativa dos mesmos). Além disso, contém um campo que armazena o tipo do ingresso comprado e, quando pressionado no botão, esse dado é transmitido para a mensagem de sucesso. 
+            <br><br>
+            4- If Else:
+            <br>
+            Para verificar se a "compra" foi realizada antes de exibir a mensagem.
+            <br><br>
+            5- Criação de terceiros:
+            <ul>
+                <li>Imagens dos ingressos - Geradas pelo ChatGPT</li>
+                <li>Informações dos ingressos - Geradas pelo ChatGPT</li>
+                <li>Paleta de cores - usada do site: https://paletadecores.com/paleta/295264/fad9a6/bd2f28/89373d/142433/</li>
+            </ul>
+        </p>
+
+        <h2>Todos: descritiva.php</h2>
+        <p>Página dedicada a cada um explicar suas funções e estruturas utilizadas junto com sua descrição.</p>
+    </div>
+</body>
+</html>
